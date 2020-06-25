cbc06bff2f025855f0ffb06f4c6593d670595352
lucascamposj <lucascj29@gmail.com>
2020-06-24 22:36:26 -0300

    Adds user tokens repository and migration

--- src/shared/infra/typeorm/migrations/1589826980176-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1589826980176-CreateUsers.ts
@@ -1,7 +1,6 @@
-import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export default class CreateUsers1589826980176 implements MigrationInterface {
-
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
@@ -12,32 +11,32 @@ export default class CreateUsers1589826980176 implements MigrationInterface {
             type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
-            default: 'uuid_generate_v4()'
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'name',
-            type: 'varchar'
+            type: 'varchar',
           },
           {
             name: 'email',
             type: 'varchar',
-            isUnique: true
+            isUnique: true,
           },
           {
             name: 'password',
-            type: 'varchar'
+            type: 'varchar',
           },
           {
             name: 'created_at',
             type: 'timestamp',
-            default: 'now()'
+            default: 'now()',
           },
           {
             name: 'updated_at',
             type: 'timestamp',
-            default: 'now()'
-          }
-        ]
+            default: 'now()',
+          },
+        ],
       })
     );
   }
--- src/shared/infra/typeorm/migrations/1593047741315-CreateUserTokens.ts
+++ src/shared/infra/typeorm/migrations/1593047741315-CreateUserTokens.ts
@@ -1,11 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppointments1589341521761
-  implements MigrationInterface {
+export class CreateUserTokens1593047741315 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'user_tokens',
         columns: [
           {
             name: 'id',
@@ -15,12 +14,14 @@ export default class CreateAppointments1589341521761
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
-            type: 'varchar',
+            name: 'token',
+            type: 'uuid',
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
+            name: 'user_id',
+            type: 'uuid',
           },
           {
             name: 'created_at',
@@ -33,11 +34,21 @@ export default class CreateAppointments1589341521761
             default: 'now()',
           },
         ],
+        foreignKeys: [
+          {
+            name: 'TokenUser',
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            columnNames: ['user_id'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+        ],
       })
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('user_tokens');
   }
 }
