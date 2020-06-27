d3f086881f6d0668d58775d4ce04feb3782e9f05
lucascamposj <lucascj29@gmail.com>
2020-06-27 20:34:45 -0300

    Adds appointments scheduling features

---
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+
+import ensureAuthenticated from '@modules/users/infra/http/middleware/ensureAuthenticated';
+import ProvidersController from '../controllers/ProvidersController';
+import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityController';
+import ProviderMonthAvailabilityController from '../controllers/ProviderMonthAvailabilityController';
+
+const providersRouter = Router();
+const providersController = new ProvidersController();
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
+
+providersRouter.use(ensureAuthenticated);
+
+providersRouter.get('/', providersController.index);
+
+providersRouter.get(
+  '/:provider_id/day-availability',
+  providerDayAvailabilityController.index
+);
+providersRouter.get(
+  '/:provider_id/month-availability',
+  providerMonthAvailabilityController.index
+);
+
+export default providersRouter;
--- src/modules/users/repositories/fakes/FakeUsersRepository.ts
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -1,6 +1,7 @@
 import IUserRepository from '@modules/users/repositories/IUsersRepository';
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
 import { uuid } from 'uuidv4';
+import IFindAllProvidersDTO from '@modules/users/dtos/IFindProvidersDTO';
 import User from '../../infra/typeorm/entities/User';
 
 class UsersRepository implements IUserRepository {
@@ -15,6 +16,17 @@ class UsersRepository implements IUserRepository {
     const findUser = this.users.find(user => user.email === email);
 
     return findUser;
+  }
+
+  public async findAllProviders({
+    except_user_id,
+  }: IFindAllProvidersDTO): Promise<User[]> {
+    let { users } = this;
+
+    if (except_user_id) {
+      users = this.users.filter(user => user.id !== except_user_id);
+    }
+    return users;
   }
 
   public async create(userData: ICreateUserDTO): Promise<User> {
