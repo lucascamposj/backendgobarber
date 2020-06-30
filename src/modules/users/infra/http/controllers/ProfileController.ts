19c66619d62c6fd0a18f7d343bc63f2fcc5dee8f
lucascamposj <lucascj29@gmail.com>
2020-06-29 21:54:34 -0300

    Adds class transformer

--- src/modules/users/infra/http/controllers/ProfileController.ts
+++ src/modules/users/infra/http/controllers/ProfileController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import UpdateProfileService from '@modules/users/services/UpdateProfileService';
 import ShowProfileService from '@modules/users/services/ShowProfileService';
@@ -12,8 +13,8 @@ export default class ProfileController {
     const showProfile = container.resolve(ShowProfileService);
 
     const user = await showProfile.execute({ user_id });
-    delete user.password;
-    return response.json(user);
+
+    return response.json(classToClass(user));
   }
 
   public async update(request: Request, response: Response): Promise<Response> {
@@ -30,8 +31,6 @@ export default class ProfileController {
       old_password,
     });
 
-    delete user.password;
-
-    return response.json(user);
+    return response.json(classToClass(user));
   }
 }
--- src/modules/users/infra/http/controllers/UserAvatarController.ts
+++ src/modules/users/infra/http/controllers/UserAvatarController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 
@@ -13,6 +14,6 @@ export default class UserAvatarController {
       avatarFilename: request.file.filename,
     });
 
-    return response.json(user);
+    return response.json(classToClass(user));
   }
 }
