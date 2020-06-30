eb06d1ba4b95d281f90023932439b97f87b02c83
lucascamposj <lucascj29@gmail.com>
2020-06-29 21:27:56 -0300

    Adds validation feature

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
+
 import ensureAuthenticated from '@modules/users/infra/http/middleware/ensureAuthenticated';
 
 import ProfileController from '../controllers/ProfileController';
@@ -8,6 +10,18 @@ const profileController = new ProfileController();
 
 profileRouter.use(ensureAuthenticated);
 profileRouter.get('/', profileController.show);
-profileRouter.put('/', profileController.update);
+profileRouter.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      old_password: Joi.string(),
+      password: Joi.string().required(),
+      password_confirmation: Joi.string().valid(Joi.ref('password')),
+    },
+  }),
+  profileController.update
+);
 
 export default profileRouter;
